56b45c9a60dfda6b5619a416c9d95bda1cac2bcd
Seda145 <royict@hotmail.com>
2023-08-24 04:37:53 +0200

    Working full version of the exam program. Ready to add real data.

--- app.js
+++ app.js
@@ -1,7 +1,74 @@
 /**[[ Copyright Roy Wierer (Seda145) ]]**/
 
 
-const AppModule = (() => {
+const App = (() => {
+
+	const QuestionCreationUtils = (() => {
+		/* Functions */
+
+		const _GetTestData = function() {
+			return {
+				Questions: [
+					{
+						Question: "Apple is a fruit",
+						RightAnswers: [
+							"Yes"
+						],
+						WrongAnswers: [
+							"No"
+						]
+					},
+					{
+						Question: "There are 30 seconds in a minute",
+						RightAnswers: [
+							"No"
+						],
+						WrongAnswers: [
+							"Yes"
+						]
+					},
+					{
+						Question: "Ants can fly",
+						RightAnswers: [
+							"No"
+						],
+						WrongAnswers: [
+							"Yes"
+						]
+					},
+					{
+						Question: "Green is blue",
+						RightAnswers: [
+							"No"
+						],
+						WrongAnswers: [
+							"Yes",
+							"Not always",
+							"No idea"
+						]
+					},
+					{
+						Question: "What is right?",
+						RightAnswers: [
+							"Cats can be white",
+							"Cats can be brown",
+						],
+						WrongAnswers: [
+							"Cats can be purple",
+						]
+					}
+				]
+			}
+		};
+
+		/* Public */
+		
+		return {
+			/* Functions */
+			GetTestData : _GetTestData,
+		};
+
+	})();
 
 
 	const UIUtils = (() => {
@@ -53,16 +120,150 @@ const AppModule = (() => {
 
 	})();
 
+	
+	const StringUtils = (() => {
+		const _StripEmptyNewLines = function(InString) {
+			return InString.replace(/(?:(?:\r\n|\r|\n)\s*){2}/gm, "\r\n");
+		}
+
+		const _StripHTML = function(InString) {
+			return InString.replace(/<\/?[^>]+(>|$)/g, "");
+		};
+
+		/* Public */
+		
+		return {
+			/* Functions */
+			StripEmptyNewLines : _StripEmptyNewLines, 
+			StripHTML : _StripHTML, 
+		};
+		
+	})();
+	
+
+	const CreationForm = (() => {
+		/* Elements */
+
+		let _eCreationForm;
+		let _eTextareaCreationInput;
+		let _eQuestionFieldsWrap;
+		// Injection buttons
+		let _eButtonInjectTestdata;
+		
+		/* Functions */
+
+		const _IsValid = function() {
+			if (!_eCreationForm 
+				|| !_eTextareaCreationInput
+				|| !_eQuestionFieldsWrap
+				// Injection buttons
+				|| !_eButtonInjectTestdata
+				) {
+				console.log("invalid form element(s).");
+				return false;
+			}
+			return true;
+		};
+
+		const _SetElementVariables = function() {
+			_eCreationForm = document.getElementById("creation-form");
+			_eTextareaCreationInput = document.getElementById("textarea-creation-input");
+			_eQuestionFieldsWrap = document.getElementById("question-fields-wrap");
+			// Injection buttons
+			_eButtonInjectTestdata = document.getElementById("creation-form-button-inject-testdata");
+		};
+
+		const _Register = function() {
+			_SetElementVariables();
+			if (!_IsValid()) {
+				return;
+			}
+
+			// Override form submit.
+			_eCreationForm.addEventListener(
+				"submit",
+				function (e) {
+					e.preventDefault();
+
+					// Just grab the value directly from the textarea, no need to process the form itself.
+					const json = JSON.parse(_eTextareaCreationInput.value);
+
+					console.log("Retrieved data from form:");
+					console.log(json);
+
+					// Empty any present questions on the question form, since they will be created from the new data.
+					_eQuestionFieldsWrap.innerHTML = "";
+					let newHTML = '';
+
+					let questionIndex = 0;
+					for (const questionX of json.Questions) {
+						newHTML += '<fieldset class="fieldstyle">';
+
+						const legend = '<legend>' + StringUtils.StripHTML(questionX.Question) + '</legend>';
+						newHTML += legend;
+
+						const answers = questionX.RightAnswers.concat(questionX.WrongAnswers);
+						const inputType = questionX.RightAnswers.length > 1 ? 'checkbox' : 'radio';
+
+						let answerIndex = 0;
+						for (const answerX of answers) {
+							newHTML += '<div class="row"><div class="col-12">';
+
+							const answerID = 'radio-question-' + questionIndex + '-answer-' + answerIndex;
+							const answerType = questionX.RightAnswers.includes(answerX) ? 'right-answer' : 'wrong-answer';
+
+							const answerString = '<input type="' + inputType + '" id="' + answerID + '" name="question-' + questionIndex + '-answer" class="' + answerType +'" value="' + answerIndex + '"></input>';
+							newHTML += answerString;
+							const answerlabel = '<label for="' + answerID + '">' + StringUtils.StripHTML(answerX) + '</label>';
+							newHTML += answerlabel;
+
+							newHTML += '</div></div>';
+							answerIndex++;
+						}
+
+						newHTML += '</fieldset>';
+						questionIndex++;
+					}
+
+					_eQuestionFieldsWrap.insertAdjacentHTML('afterbegin', newHTML);
+
+					Navigation.NavigateTo(1);
+
+					return false;
+				},
+				false
+			);
+
+			// Bind injection button functionality.
+
+			_eButtonInjectTestdata.addEventListener("click", function() {
+				console.log(QuestionCreationUtils.GetTestData());
+				_eTextareaCreationInput.value = JSON.stringify(QuestionCreationUtils.GetTestData(), null, 2);
+			}); 
+		};
+		
+		/* Public */
+		
+		return {
+		  /* Functions */
+		  IsValid : _IsValid,
+		  Register : _Register
+		};
+	
+	})();
+
 
 	const QuestionForm = (() => {
 		/* Elements */
 
 		let _eQuestionForm;
+		let _eQuestionFieldsWrap;
 		
 		/* Functions */
 
 		const _IsValid = function() {
 			if (!_eQuestionForm 
+				|| !_eQuestionFieldsWrap
 				) {
 				console.log("invalid form element(s).");
 				return false;
@@ -72,6 +273,7 @@ const AppModule = (() => {
 
 		const _SetElementVariables = function() {
 			_eQuestionForm = document.getElementById("question-form");
+			_eQuestionFieldsWrap = document.getElementById("question-fields-wrap");
 		};
 
 		const _Register = function() {
@@ -88,6 +290,10 @@ const AppModule = (() => {
 					const formData = new FormData(_eQuestionForm);
 
 					// DOSTUFF
+					console.log("Question form posts data:");
+					console.log(formData);
+
+					Navigation.NavigateTo(2);
 
 					return false;
 				},
@@ -98,9 +304,11 @@ const AppModule = (() => {
 		/* Public */
 		
 		return {
-		  /* Functions */
-		  IsValid : _IsValid,
-		  Register : _Register
+			/* Elements */
+			QuestionFieldsWrap : _eQuestionFieldsWrap,
+			/* Functions */
+			IsValid : _IsValid,
+			Register : _Register
 		};
 	
 	})();
@@ -110,13 +318,15 @@ const AppModule = (() => {
 		/* Elements */
 
 		let _eTabs;
-		let _eAnchorQuestions;
+		let _eCreationContent;
+		let _eQuestionContent;
 
 		/* Functions */
 
 		const _IsValid = function() {
 			if (!_eTabs
-				|| !_eAnchorQuestions
+				|| !_eCreationContent
+				|| !_eQuestionContent
 				) {
 				console.log("invalid navigation element(s).");
 				return false;
@@ -126,7 +336,8 @@ const AppModule = (() => {
 
 		const _SetElementVariables = function() {
 			_eTabs = document.getElementById("tabs");
-			_eAnchorQuestions = document.getElementById("anchor-questions");
+			_eCreationContent = document.getElementById("creation-content");
+			_eQuestionContent = document.getElementById("question-content");
 		};
 
 		const _Register = function() {
@@ -148,20 +359,28 @@ const AppModule = (() => {
 		};
 
 		const _NavigateTo = function(InContentId) {
-			let eAnchor = null;
 			switch (Number(InContentId)) {
 				case 0:
-					eAnchor = _eAnchorQuestions;
+					UIUtils.UpdateVisibility(_eCreationContent, true);
+					UIUtils.UpdateVisibility(_eQuestionContent, false);
+					_eQuestionContent.classList.remove("show-results");
 				break;
-			}
+				case 1:
+					UIUtils.UpdateVisibility(_eCreationContent, false);
+					UIUtils.UpdateVisibility(_eQuestionContent, true);
+					_eQuestionContent.classList.remove("show-results");
 
-			if (eAnchor == null) {
-				console.log("Navigation error, eAnchor == null");
-				return;
+				break;
+				case 2:
+					UIUtils.UpdateVisibility(_eCreationContent, false);
+					UIUtils.UpdateVisibility(_eQuestionContent, true);
+					_eQuestionContent.classList.add("show-results");
+				break;
+				default:
+					console.log("Navigation error, this contentId is not used.");
+					return;
 			}
-			// console.log("Navigating to InContentId: " + InContentId);
-
-			eAnchor.scrollIntoView({behavior: "smooth"});
+			console.log("Navigated to InContentId: " + InContentId);
 		};
 
 		/* Public */
@@ -180,9 +399,12 @@ const AppModule = (() => {
 
 	const _StartModule = (() => {
 		// Register the forms
+		CreationForm.Register();
 		QuestionForm.Register();
 		// Register the navigation module.
 		Navigation.Register();
+		// Initial navigation.
+		Navigation.NavigateTo(0);
 	})();
   
 })();
